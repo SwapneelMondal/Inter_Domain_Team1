c2b6a61c96d27844c73533ba396f20220ad910f9
SwapneelMondal <swapneelmondal587@gmail.com>
2025-09-21 00:22:22 +0530

    Updated Servo Test code

--- servo.ino
+++ servo.ino
@@ -1,6 +1,7 @@
 #include <Servo.h>
 Servo s1;
 int servoPin=9;
+
 void setup() {
  s1.attach(servoPin);
  Serial.begin(9600);
@@ -8,7 +9,7 @@ void setup() {
 
 void loop() {
 s1.write(45);
-for(int i=0;i<90;i++){
+for(int i=0;i<=90;i++){
   s1.write(i+45);
   delay(50);
 }
