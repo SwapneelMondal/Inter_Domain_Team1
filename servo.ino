16c61a63c97e164f41b5f5ac3127d5d74f887ef0
SwapneelMondal <swapneelmondal587@gmail.com>
2025-09-21 00:18:36 +0530

    Added Servo Motor test code

---
+++ servo.ino
@@ -0,0 +1,17 @@
+#include <Servo.h>
+Servo s1;
+int servoPin=9;
+void setup() {
+ s1.attach(servoPin);
+ Serial.begin(9600);
+}
+
+void loop() {
+s1.write(45);
+for(int i=0;i<90;i++){
+  s1.write(i+45);
+  delay(50);
+}
+
+
+}
