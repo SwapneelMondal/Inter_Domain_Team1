a6a13332c109841819ea05600fa81b5d1259ad6e
SwapneelMondal <swapneelmondal587@gmail.com>
2025-09-23 05:50:51 +0530

    Main mechanism code updated

--- mechanism.ino
+++ mechanism.ino
@@ -1,73 +1,92 @@
 #include <Servo.h>
 #include <LiquidCrystal_I2C.h>
 
-Servo s1, s2, s3, s4;
 LiquidCrystal_I2C lcd(0x20, 16, 2);
+
+Servo s1;
+Servo s2;
+Servo s3;
+
+int amazonBox = 0;
+int flipkartBox = 0;
+int myntraBox = 0;
+
+int in1 = 12;
+int in2 = 13;
+int en  = 10;
+
 String input;
 
 void setup() {
-  s1.attach(11);
-  s2.attach(10);
-  s3.attach(9);
-  s4.attach(6);
-
-  Serial.begin(9600);
-
-  lcd.init();
-  lcd.backlight();
-  lcd.clear();
+  s1.attach(6);
+  s2.attach(5);
+  s3.attach(3);
 
   s1.write(0);
   s2.write(0);
   s3.write(0);
-  s4.write(0);
 
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(en, OUTPUT);
+
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  analogWrite(en, 255);
+
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("Robot Ready");
+
+  Serial.begin(9600);
   Serial.println("Enter company name: ");
-  lcd.setCursor(0, 0);
-  lcd.print("Enter company:");
 }
 
 void loop() {
   if (Serial.available()) {
     input = Serial.readStringUntil('\n');
+    lcd.clear();
 
     if (input == "Amazon") {
-      for (int i = 0; i <= 90; i++) {
-        s1.write(i);
-        delay(15);
-      }
-      lcd.clear();
-      lcd.print("Amazon box");
-    } 
-    else if (input == "Flipkart") {
-      for (int i = 0; i <= 90; i++) {
-        s2.write(i);
-        delay(15);
-      }
-      lcd.clear();
-      lcd.print("Flipkart box");
-    } 
-    else if (input == "Blinkit") {
-      for (int i = 0; i <= 90; i++) {
-        s3.write(i);
-        delay(15);
-      }
-      lcd.clear();
-      lcd.print("Blinkit box");
-    } 
-    else if (input == "Myntra") {
-      for (int i = 0; i <= 90; i++) {
-        s4.write(i);
-        delay(15);
-      }
-      lcd.clear();
-      lcd.print("Myntra box");
-    } 
-    else {
-      Serial.println("Invalid Input");
-      lcd.clear();
-      lcd.print("Invalid Input");
+      s1.write(90);
+      lcd.setCursor(0,0);
+      lcd.print("Amazon Box");
+      amazonBox++;
+      Serial.print(amazonBox);
+      Serial.println(" Amazon boxes detected");
+
+      delay(7000); 
+      s1.write(0); 
     }
-   
+    else if (input == "Flipkart") {
+      s2.write(90); 
+      lcd.setCursor(0,0);
+      lcd.print("Flipkart Box");
+      flipkartBox++;
+      Serial.print(flipkartBox);
+      Serial.println(" Flipkart boxes detected");
+
+      delay(5000); 
+      s2.write(0);   
+    }
+    else if (input == "Myntra") {
+      s3.write(90);  
+      lcd.setCursor(0,0);
+      lcd.print("Myntra Box");
+      myntraBox++;
+      Serial.print(myntraBox);
+      Serial.println(" Myntra boxes detected");
+
+      delay(5000);   
+      s3.write(0);  
+    }
+    else {
+      lcd.setCursor(0,0);
+      lcd.print("Invalid Input");
+      Serial.println("Invalid Input");
+    }
+
+    Serial.println("Enter company name: ");
   }
 }
